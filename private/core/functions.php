def7428c632beb848158dcd560ce77e7abd37c46
Henry Vaughn <hcvaughn@unca.edu>
2025-01-19 18:19:43 -0500

    fix: remove duplicate display_session_message function
    The display_session_message function was moved to error_functions.php but was
    still present in functions.php, causing a 'Cannot redeclare' error. This commit
    removes the duplicate function.

--- private/core/functions.php
+++ private/core/functions.php
@@ -41,14 +41,4 @@ if(!function_exists('money_format')) {
   }
 }
 
-function display_session_message() {
-    global $session;
-    $msg = $session->message();
-    if(isset($msg) && $msg != '') {
-        $session->clear_message();
-        return '<div class="message">' . h($msg) . '</div>';
-    }
-    return '';
-}
-
 ?>
